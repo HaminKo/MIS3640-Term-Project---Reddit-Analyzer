6195e2d144ad1677e19b5eb803a32a3657b4b9c4
Ha Min Ko <hko1@babson.edu>
2018-12-07 12:47:45 -0500

    split script

--- reddit_analyzer/analyze.py
+++ reddit_analyzer/analyze.py
@@ -16,8 +16,33 @@ def index():
     """
     return render_template('analysis/index.html')
 
-@bp.route('/handle_data', methods=['POST'])
-def handle_data():
+# @bp.route('/download_data', methods=['POST'])
+# def download_data():
+#     # projectpath = request.form['projectFilepath']
+#     subreddit = request.form.get('subreddit')
+#     sort = request.form.get('sort')
+#     time_period = request.form.get('time_period')
+#     limit = request.form.get('lmit')
+#     # print(subreddit, sort, time_period, limit)
+
+#     print('time 1')
+#     # Redditpraw
+#     data = rp.scrape_subreddits(subreddit, sort=sort, time_period=time_period, limit=limit)
+#     rp.save_file(data, "reddit.csv")
+
+#     print('time 2')
+#     # Redditsentiment
+#     rs.analyze_submissions("reddit.csv", "reddit.csv")
+
+#     print('time 3')
+#     # Redditanalyzer
+#     ra.create_graphs("reddit.csv")
+#     print('time 4')
+
+#     return redirect('/analysis')
+
+@bp.route('/download_data', methods=['POST'])
+def download_data():
     # projectpath = request.form['projectFilepath']
     subreddit = request.form.get('subreddit')
     sort = request.form.get('sort')
@@ -25,16 +50,26 @@ def handle_data():
     limit = request.form.get('lmit')
     # print(subreddit, sort, time_period, limit)
 
+    print('time 1')
     # Redditpraw
     data = rp.scrape_subreddits(subreddit, sort=sort, time_period=time_period, limit=limit)
     rp.save_file(data, "reddit.csv")
+    print('time 2')
+    return redirect('/analyze_data', code=307)
 
+@bp.route('/analyze_data', methods=['GET', 'POST'])
+def analyze_data():
     # Redditsentiment
     rs.analyze_submissions("reddit.csv", "reddit.csv")
+    print('time 3')
+    return redirect('graph_data', code=307)
 
+@bp.route('/graph_data', methods=['GET', 'POST'])
+def graph_data():
+    print('time 3')
     # Redditanalyzer
     ra.create_graphs("reddit.csv")
-
+    print('time 4')
     return redirect('/analysis')
 
 @bp.route('/analysis')
